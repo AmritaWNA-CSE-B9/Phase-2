d7c6fff83e3fbd26923901a00e326ae52d72bccb
Karthik-PM <karthikpm52@gmail.com>
2024-02-11 13:55:24 +0530

    Sperical warping test

---
+++ main/main.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <opencv2/opencv.hpp>
+#include "./headers/cylendricalWarper.h"
+#include "./headers/homographer.h"
+
+int main(int argc, char const *argv[])
+{
+    cv::Mat Left, Mid, Right;
+    return 0;
+}
